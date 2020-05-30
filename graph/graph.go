87248ba59073b0b5ac6bf6933bdefa1142015e68
nusfie12345 <dumatarik903@gmail.com>
2020-05-30 20:40:04 +0300

    Added ScaleForPlayer and ScaleForLocation - coordinate converters.

--- graph/graph.go
+++ graph/graph.go
@@ -3,6 +3,7 @@ package graph
 import (
 	"image/color"
 	"gopkg.in/fogleman/gg.v1"
+	"team2_qgame/models"
 )
 //S equals width and heigth of map image. Made as float64 to use it in drawing funcs.
 const S float64 = 1024
@@ -25,4 +26,26 @@ func GetMapGrid() {
 	}
 	dc.SavePNG("gamemap_grid.png")
 	//TO DO: get filepath.Abs("./gamemap_grid.png") and make tg bot send it as PhotoConfig or sth like that.
+}
+
+//ScaleForPlayer converts players game coordinates into pixels for map.
+func ScaleForPlayer(int, int) (float64, float64) {
+	p := models.Player{}
+	mappX := S/9*float64(p.X) //<- fancy double P cuz forgot that i had to do same for locations.
+	mappY := S/9*float64(p.Y)
+	return mappX, mappY
+}
+
+//ScaleForLocation - pretty much like ScaleForPlayer, but for locations. tbh much harder((
+func ScaleForLocation(int, int) (float64, float64) {
+	var (loc models.Location	
+	 x, y int)
+
+	m := models.Matrix{}
+	loc = m.GetLocation(x, y)
+	locX, locY := models.Location.GetLocation(loc)
+	maplX := S/9*float64(locX)
+	maplY := S/9*float64(locY)
+	return maplX, maplY
+	//WIP: somehow connect those variables x and y to actual coordinates. By far no clue about that.
 }
