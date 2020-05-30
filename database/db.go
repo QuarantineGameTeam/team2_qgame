e3c0bad463b73c6c2d14295ef9726aba22eef27f
Roman Tkachenko <rt4freel@gmail.com>
2020-05-31 00:04:07 +0300

    Added DBHandler and default constructor

--- database/db.go
+++ database/db.go
@@ -1,1 +1,23 @@
 package database
+
+import (
+	"database/sql"
+	//import sqlite driver
+	_ "github.com/mattn/go-sqlite3"
+)
+
+//DBHandler holds interfaces to interact with database
+type DBHandler struct {
+	DriverName string
+	DBPath     string
+	Connection *sql.DB
+}
+
+//Connect creates connection with database file by driver
+func (dbh *DBHandler) Connect() {
+	var err error
+	dbh.Connection, err = sql.Open(dbh.DriverName, dbh.DBPath)
+	if err != nil {
+		panic(err)
+	}
+}
