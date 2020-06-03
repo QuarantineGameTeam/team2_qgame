eae1720b83e58da469751bac2f43b0ec4e9a72bd
Roman Tkachenko <rt4freel@gmail.com>
2020-06-03 10:33:32 +0300

    Added Update function for database

--- api/user.go
+++ api/user.go
@@ -1,9 +1,10 @@
 package api
 
 type User struct {
-	ID int `json:"id"`
-	IsBot bool `json:"is_bot"`
+	ID        int    `json:"id"`
+	State     int    `json:"state"`
+	IsBot     bool   `json:"is_bot"`
 	FirstName string `json:"first_name"`
-	Username string `json:"username"`
-	LangCode string `json:"language_code"`
+	Username  string `json:"username"`
+	LangCode  string `json:"language_code"`
 }
--- database/db.go
+++ database/db.go
@@ -30,7 +30,8 @@ func (dbh *DBHandler) CreateUsersTable() {
 	_, err := dbh.Connection.Exec(
 		`CREATE TABLE IF NOT EXISTS users (
     		   		telegram_id INTEGER UNIQUE PRIMARY KEY,
-    				nickname TEXT);`)
+					nickname TEXT,
+					state INTEGER);`)
 	if err != nil {
 		panic(err)
 	}
@@ -39,15 +40,15 @@ func (dbh *DBHandler) CreateUsersTable() {
 //InsertUser adds a user to the Users table
 func (dbh *DBHandler) InsertUser(user api.User) {
 	//User structure is described in the api package file user.go
-	_, err := dbh.Connection.Exec(`INSERT INTO users (telegram_id, nickname) VALUES (?, ?);`, user.ID, user.Username)
+	_, err := dbh.Connection.Exec(`INSERT INTO users (telegram_id, nickname, state) VALUES (?, ?, ?);`, user.ID, user.Username, user.State)
 	if err != nil {
 		panic(err)
 	}
 }
 
-//UpdateUser updates user with specified id
-func (dbh *DBHandler) UpdateUser(user api.User, newNickname string) {
-	_, err := dbh.Connection.Exec(`UPDATE users SET nickname = ? WHERE telegram_id = ?;`, newNickname, user.ID)
+//Update updates any field in any table with new value
+func (dbh *DBHandler) Update(table, field string, value interface{}, whereField string, whereValue interface{}) {
+	_, err := dbh.Connection.Exec(`UPDATE ? SET ? = ? WHERE ? = ?`, table, field, value, whereField, whereValue)
 	if err != nil {
 		panic(err)
 	}
@@ -66,7 +67,7 @@ func (dbh *DBHandler) NameExists(name string) bool {
 	return false
 }
 
-//NameExists returns true if a user with the same name is already registered
+//ContainsUser returns true if a user with the same name is already registered
 func (dbh *DBHandler) ContainsUser(user api.User) bool {
 	result, err := dbh.Connection.Query(`SELECT * FROM users WHERE telegram_id = ?;`, user.ID)
 	if err != nil {
@@ -88,7 +89,7 @@ func (dbh *DBHandler) GetUserByID(id int) *api.User {
 	}
 	defer result.Close()
 	if result.Next() {
-		err := result.Scan(&user.ID, &user.Username)
+		err := result.Scan(&user.ID, &user.Username, &user.State)
 		if err != nil {
 			fmt.Println(err)
 			user = &api.User{}
