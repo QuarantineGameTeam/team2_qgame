794a700bda7146e6496caccf4f4642f878dbcb28
Dan Timachov <48999054+ghadd@users.noreply.github.com>
2020-06-03 12:43:44 +0300

    Update config.go
    added StateNone

--- config.go
+++ config.go
@@ -4,9 +4,10 @@ const (
 	botToken = "1285255270:AAFdQW1_ygN6CxQU8DzRBHLS3YLaKswLdqY"
 
 	//User state constants
-	StateAFK          = iota //passive state for user
-	StateChangingName = iota //user is changing nickname
-	StateWriting      = iota //user answers the question
-	StateWaiting      = iota //user is waiting for his turn
-	StateThinking     = iota //there is user's move now
+	StateNone         = iota // no interaction possible at all
+	StateAFK          = iota // passive state for user
+	StateChangingName = iota // user is changing nickname
+	StateWriting      = iota // user answers the question
+	StateWaiting      = iota // user is waiting for his turn
+	StateThinking     = iota // there is user's move now
 )
