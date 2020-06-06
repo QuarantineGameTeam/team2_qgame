1edcdd203dce7d7d035c2614594365d395814515
Dan <danyatimachov@gmail.com>
2020-06-06 18:44:16 +0300

    Game struct

--- game/game.go
+++ game/game.go
@@ -1,1 +1,13 @@
 package game
+
+import (
+	"github.com/QuarantineGameTeam/team2_qgame/models"
+)
+
+type Game struct {
+	GameID        int
+	Locations     []models.Location
+	PlayerID      int
+	StartMoveTime int
+	GameJSON      string
+}
